b6597ce66b81cda9c7e2a100a25b965bf9677545
Sergey Fedotov <fsa@tavda.info>
2019-09-20 02:32:39 +0700

    Добавлен gulp для сборки css.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const {src, dest, watch, series, parallel} = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    bs = require('browser-sync').create();
+
+function scssTask() {
+    return src('src/scss/**/*.+(scss|sass)')
+            .pipe(sass(
+//                    {
+//                        outputStyle: 'expanded',
+//                        includePaths: ["node_modules/bootstrap/scss/"]
+//                    }
+            ))
+            .pipe(autoprefixer(['last 15 versions', '>1%', 'ie 8']))
+//    .pipe(cssnano())
+            .pipe(dest('htdocs'))
+            .pipe(bs.stream());
+};
+
+function watchTask() {
+    bs.init({
+        proxy: "neohome.net.localhost"
+    });
+    watch('src/scss/**/*.+(scss|sass)', scssTask);
+    watch('htdocs/**/*.+(php|html|css|js)', bs.reload);
+}
+
+exports.scss = scssTask;
+exports.watch = watchTask;
