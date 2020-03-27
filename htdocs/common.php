1008f02a1d242d60814e95d0c838b8c124f3dccb
Sergey Fedotov <fsa@tavda.info>
2020-03-27 20:25:42 +0700

    Исправлена ошибка в common.php

--- htdocs/common.php
+++ htdocs/common.php
@@ -3,5 +3,5 @@
 set_include_path(get_include_path().':'.__DIR__.'/classes/');
 spl_autoload_extensions('.php');
 spl_autoload_register();
-#date_default_timezone_set(Settings::get('timezone'));
-set_exception_handler(['HTML','Exception']);
+date_default_timezone_set(Settings::get('timezone'));
+httpResponse::setModeHtml();
